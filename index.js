a4f090cd1c6f524eef45a925cc13881abb63ae25
Sheikhmsip <sheikhmsip@gmail.com>
2024-09-13 22:27:21 +0600

    init server site

---
+++ index.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+// middleware 
+app.use(cors());
+app.use(express.json());
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.USER_PASS}@cluster0.u6m9y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    const newsCollection = client.db('The_Dragon_NewsPaper').collection('news');
+
+
+    app.get('/news', async(req, res) =>{
+        const news = newsCollection.find();
+        const result = await news.toArray();
+        res.send(result);
+      })
+
+      app.get('/news/:id', async (req, res) => {
+        const id = req.params.id;
+        console.log(id);
+        try {
+          const query = { _id: new ObjectId(id) };
+          console.log(query);
+          const result = await newsCollection.findOne(query);
+      
+          if (!result) {
+            return res.status(404).send({ message: 'News not found' });
+          }
+      
+          res.send(result);
+        } catch (err) {
+          console.error('Error fetching news by ID:', err);
+          res.status(500).send({ message: 'Internal Server Error', error: err });
+        }
+      });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('News is running')
+})
+
+app.listen(port, () => {
+    console.log(`News server is running on port ${port}`)
+})
