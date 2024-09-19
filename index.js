27a3b20aec1356fde76542b9a9b9090dce3a8811
Sheikhmsip <sheikhmsip@gmail.com>
2024-09-20 00:20:28 +0600

    create get, post and get by id

--- index.js
+++ index.js
@@ -54,6 +54,12 @@ async function run() {
         }
       });
 
+      app.post("/news", async(req, res)=> {
+        const newses = req.body; 
+        const result = await newsCollection.insertOne(newses)
+        res.send(result)
+      })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
