239782dd4a75d4b035e96fc7974f15229daf28bc
Sheikhmsip <sheikhmsip@gmail.com>
2024-09-21 03:48:46 +0600

    create get, put, delete function and get news by id

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
-// middleware 
+// middleware
 app.use(cors());
 app.use(express.json());
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
-const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.USER_PASS}@cluster0.u6m9y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = "mongodb+srv://The_Dragon_NewsPepar:CF9i1gmPHCjr2Mso@cluster0.vvvwsgj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,40 +25,80 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
-    const newsCollection = client.db('The_Dragon_NewsPaper').collection('news');
+    const newsCollection = client.db("The_Dragon_NewsPaper").collection("news");
 
+    app.get("/news", async (req, res) => {
+      const news = newsCollection.find();
+      const result = await news.toArray();
+      res.send(result);
+    });
 
-    app.get('/news', async(req, res) =>{
-        const news = newsCollection.find();
-        const result = await news.toArray();
-        res.send(result);
-      })
+    app.get("/news/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      try {
+        const query = { _id: new ObjectId(id) };
+        console.log(query);
+        const result = await newsCollection.findOne(query);
 
-      app.get('/news/:id', async (req, res) => {
-        const id = req.params.id;
-        console.log(id);
-        try {
-          const query = { _id: new ObjectId(id) };
-          console.log(query);
-          const result = await newsCollection.findOne(query);
-      
-          if (!result) {
-            return res.status(404).send({ message: 'News not found' });
-          }
-      
-          res.send(result);
-        } catch (err) {
-          console.error('Error fetching news by ID:', err);
-          res.status(500).send({ message: 'Internal Server Error', error: err });
+        if (!result) {
+          return res.status(404).send({ message: "News not found" });
         }
-      });
 
-      app.post("/news", async(req, res)=> {
-        const newses = req.body; 
-        const result = await newsCollection.insertOne(newses)
-        res.send(result)
-      })
+        res.send(result);
+      } catch (err) {
+        console.error("Error fetching news by ID:", err);
+        res.status(500).send({ message: "Internal Server Error", error: err });
+      }
+    });
 
+    // app.get("/editNews/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   // console.log(id);
+    //   try {
+    //     const query = { _id: new ObjectId(id) };
+    //     console.log(query);
+    //     const result = await newsCollection.findOne(query);
+
+    //     if (!result) {
+    //       return res.status(404).send({ message: "News not found" });
+    //     }
+
+    //     res.send(result);
+    //   } catch (err) {
+    //     console.error("Error fetching news by ID:", err);
+    //     res.status(500).send({ message: "Internal Server Error", error: err });
+    //   }
+    // });
+
+    app.post("/news", async (req, res) => {
+      const newses = req.body;
+      const result = await newsCollection.insertOne(newses);
+      res.send(result);
+    });
+    
+    app.delete("/news/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await newsCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.put("/news/:id", async(req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = {upsert: true};
+      const editNews= req.body;
+      const news = {
+        $set: {
+          title: editNews.title,
+          details: editNews.details,
+          image_url: editNews.image_url
+        }
+      }
+      const result = await newsCollection.updateOne(filter, news, options);
+      res.send(result)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -71,12 +110,10 @@ async function run() {
 run().catch(console.dir);
 
 
-
-
-app.get('/', (req, res) => {
-    res.send('News is running')
-})
+app.get("/", (req, res) => {
+  res.send("News is running");
+});
 
 app.listen(port, () => {
-    console.log(`News server is running on port ${port}`)
-})
+  console.log(`News server is running on port ${port}`);
+});
