87a797474dcf2106e808b3face540b7c2723609a
Sheikhmsip <sheikhmsip@gmail.com>
2024-10-01 00:08:16 +0600

    change mongodb user and password

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ app.use(express.json());
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.USER_PASS}@cluster0.vvvwsgj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.USER_PASS}@cluster0.vvvwsgj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.USER_PASS}@cluster0.u6m9y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
